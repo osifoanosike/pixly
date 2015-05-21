94a16a83d2eef51e6a58e9ad34dcac9252b1f3cc
anosikeosifo <appdev.anosikeosifo@gmail.com>
2015-05-21 14:45:02 +0100

    UI Improvement iteration

--- app/assets/javascripts/photos.js
+++ app/assets/javascripts/photos.js
@@ -1,9 +1,18 @@
-function GalleryPhoto(guest_like_btn, like_form) {
+function GalleryPhoto(guest_like_btn, like_form, share_btn, share_view) {
   this.guest_like_btn = guest_like_btn;
   this.like_form = like_form;
+  this.share_btn = share_btn;
+  this.share_view = share_view;
 }
 
 GalleryPhoto.prototype = {
+
+  init: function() {
+    //hide the sbuton for sharing images
+    $('#send_invite_btn').hide();
+
+    this.addEventHandlers();
+  },
 
   set_form_param: function(like_button){
     this.like_form.find('#like_photo_id').val(like_button.data('photo-id'));
@@ -17,7 +26,13 @@ GalleryPhoto.prototype = {
     this.like_form.modal('hide');
   },
 
-  addEventHandler: function() {
+  show_share_view: function(current_button) {
+    this.share_view.find('#share_image_url').val(current_button.data('image'));
+    this.share_view.find('#image').attr('src', current_button.data('image'));
+    this.share_view.modal('show');
+  },
+
+  addEventHandlers: function() {
     var that = this;
 
     this.guest_like_btn.click(function(){
@@ -28,6 +43,10 @@ GalleryPhoto.prototype = {
     this.like_form.find('#like_photo_btn').click(function(){
       that.close_like_form();
     });
+
+    this.share_btn.click(function() {
+      that.show_share_view($(this));
+    });
   }
 }
 
@@ -36,7 +55,9 @@ GalleryPhoto.prototype = {
 $(document).on('page:change', function(){
   var guest_like_btn = $('button.guest_like_btn');
   var like_form = $('#like_photo_form');
+  var share_view = $('#share_image_view');
+  var share_btn = $('button.share_photo')
 
-  var photo = new GalleryPhoto(guest_like_btn, like_form);
-  photo.addEventHandler();
+  var photo = new GalleryPhoto(guest_like_btn, like_form, share_btn, share_view);
+  photo.init();
 });
